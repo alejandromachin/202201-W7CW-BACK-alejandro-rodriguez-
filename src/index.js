f1268655df8a09b34b3e9bd68ce3f222227ad7fc
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-25 18:15:40 +0100

    added iffe function to call serverUp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 require("dotenv").config();
-const debug = require("debug")("network:server");
+const app = require("./server");
+const serverUp = require("./server/serverUp");
 
-const serverUp = (port, app) =>
-  new Promise((resolve, reject) => {
-    const server = app.listen(port, () => {
-      debug(`We are running on port http://localhost:${port}`);
-      resolve();
-    });
-    server.on("error", (error) => {
-      const message =
-        error.code === "EADDRINUSE" ? `Port ${port} busy` : error.message;
-      reject(new Error(`Error on server: ${message}`));
-    });
-  });
+const port = process.env.PORT || 4000;
 
-module.exports = serverUp;
+(async () => {
+  await serverUp(port, app);
+})();
