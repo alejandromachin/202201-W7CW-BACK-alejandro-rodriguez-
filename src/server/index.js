67d5e29d289ebb0782a0ef8ab0245cca8843dde3
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-25 19:14:01 +0100

    added errors handler with middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const { generalError, notFoundError } = require("./middlewares/errors");
 
 const app = express();
 app.use(morgan("dev"));
 
+app.use(notFoundError);
+app.use(generalError);
 module.exports = app;
