3c15e43b114a71c941baabed4808b3273d789ee5
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 14:16:00 +0100

    added cors package and middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const { generalError, notFoundError } = require("./middlewares/errors");
 const accessRouter = require("./routers/accessRouter");
@@ -7,6 +8,14 @@ const accessRouter = require("./routers/accessRouter");
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use("/login", accessRouter);
 
