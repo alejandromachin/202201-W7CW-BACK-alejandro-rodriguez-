e4767654f31a90bead2bec67c25497d85a4b242f
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 15:44:05 +0100

    added a registerUser middleware

--- src/server/routers/accessRouter.js
+++ src/server/routers/accessRouter.js
@@ -4,6 +4,7 @@ const { loginUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/", loginUser);
+router.post("/login", loginUser);
+router.post("/register", loginUser);
 
 module.exports = router;
