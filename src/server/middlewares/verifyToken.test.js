0ae8405e4183d2887bdb1f375c0b1427c796a134
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 12:05:33 +0100

    added express.json and verifytoken ok test

---
+++ src/server/middlewares/verifyToken.test.js
@@ -0,0 +1,23 @@
+const verifyToken = require("./verifyToken");
+
+require("dotenv").config();
+
+describe("given a verifyToken ", () => {
+  describe("When it receives a response with a valid token", () => {
+    test("Then it should call its next method", async () => {
+      const req = {
+        header: jest
+          .fn()
+          .mockReturnValue(
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hY2hpbmF6byIsImlkIjoiNjIxOTEzMGFhNDU1NDZlMTU5YjI0NzRmIiwiaWF0IjoxNjQ1ODczMzA2fQ.Hvs9iJROuLyh3cSorjvu8SrL5ofUhrwVe-VEgVJbjgI"
+          ),
+      };
+
+      const next = jest.fn();
+
+      await verifyToken(req, null, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
