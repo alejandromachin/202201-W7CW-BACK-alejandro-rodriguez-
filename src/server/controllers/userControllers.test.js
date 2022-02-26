46e05944da19a5710fd35234e40f27ca89b7fab0
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-26 11:19:18 +0100

    added getlogin test for error

---
+++ src/server/controllers/userControllers.test.js
@@ -0,0 +1,24 @@
+const User = require("../../database/models/User");
+const { loginUser } = require("./userControllers");
+
+jest.mock("../../database/models/User");
+
+describe("Given a getLogin function", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  describe("When it receives a response", () => {
+    test("Then if the user does not exist it should throw an error with the status code 404 and the error message 'Username or password are wrong'", async () => {
+      const req = {
+        body: { username: "machinazo", password: "123" },
+      };
+      const next = jest.fn();
+      const error = new Error("Username or password are wrong");
+      User.find = jest.fn().mockRejectedValue(null);
+
+      await loginUser(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
